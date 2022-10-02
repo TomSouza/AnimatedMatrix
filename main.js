3417efc39b1634ec6181d0f082019439e4634dc5
Thomas Souza <thomas.aes.31@hotmail.com>
2022-10-01 23:00:30 -0300

    Animações
    Cria animações com CSS e gera loop para ativa-las por JavaScript.

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
 const body = document.body;
-const w = 50;
-const h = 50;
-const matriz = [25,25];
+const width = 50;
+const height = 50;
+const matriz = [15,15];
+
+const timer = (seconds) =>  {
+    let time = seconds * 1000
+    return new Promise(res => setTimeout(res, time))
+}
+
+function appendCube(row, column) {
+    image = document.createElement('img');
+    image.src = "Images/Cube.png";
+    image.classList.add("cube");
+
+    image.style.height = height + "px";
+    image.style.width = width + "px";
+
+    image.style.left = ((column * width) /2) + columnOffset + window.innerWidth /2 + "px";
+    image.style.top = (row * (height/4)) + rowOffset + "px";
+
+    rowOffset += (height/4);
+
+    image.id = row +"_"+ column;
+
+    body.append(image);
+}
 
 columnOffset = 0;
 for (let row = 1; row <= matriz[0]; row++) {
     rowOffset = 0;
     for (let column = 1; column <= matriz[1]; column++) {
-        
-        image = document.createElement('img');
-        image.src = "Images/Cube.png";
-        image.classList.add("cube");
-
-        image.style.height = h + "px";
-        image.style.width = w + "px";
-
-        image.style.left = ((column * w) /2) + columnOffset + window.innerWidth /2 + "px";
-        image.style.top = (row * (h/4)) + rowOffset + "px";
-
-        rowOffset += (h/4);
-        
-        image.id = row +"_"+ column;
-        
-        body.append(image);
+        appendCube(row, column);
     }
-    columnOffset -= (w/2);
+    columnOffset -= (width/2);
 }
+
+async function secondWave(row) {
+    for (let column = 1; column <= matriz[1]; column++) {
+        document.getElementById(row +"_"+ column).classList.add("hoverClass");
+        await timer(1);
+        document.getElementById(row +"_"+ column).classList.remove("hoverClass");
+    }
+    wave();
+}
+
+async function wave() {
+    for (let row = 1; row <= matriz[0]; row++) {
+        await timer(0.1);
+        secondWave(row);
+    }
+}
+
+wave();
